3007f81814825d27c656dbfbdac0fe823c61e87e
ckohen <chaikohen@gmail.com>
2022-06-22 21:30:00 -0700

    feat: export discord-api-types

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { default, default as deploy } from './lib/Deploy';
 export * from './lib/Deploy';
 export * from './bin/deploy-interactions';
 export * from './lib/Util';
+export * from 'discord-api-types/v10';
